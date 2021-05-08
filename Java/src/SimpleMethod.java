7b67f439da66e010cc79e6032ccc0d76a5d6d476
Amado Azua III <amado.azua3@gmail.com>
2021-05-08 14:47:23 -0500

    Java method hierarchy

---
+++ Java/src/SimpleMethod.java
@@ -0,0 +1,29 @@
+/**
+ * This program defines and calls a simple method
+ */
+
+public class SimpleMethod {
+
+    public static void main(String[] args){
+
+        System.out.println("Hello from the main method");
+        displayMessage();
+        System.out.println("Back in the main method.");
+    }
+
+    /**
+     * The displayMessage method displays a greeting
+     */
+
+    public static void displayMessage(){
+
+        System.out.println("Hello from the displayMessage method.");
+        anotherMessage();
+    }
+
+    public static void anotherMessage(){
+
+        System.out.println("Hello from anotherMessage method.");
+
+    }
+}
